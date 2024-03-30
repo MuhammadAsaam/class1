ebcb4ee7c492d1729b4470d6ce2eb1e842197590
MuhammadAsaam <mrasaam4@gmail.com>
2024-03-30 22:44:46 +0500

    my first class

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log("Hello World!");
+// console.log("Hello world!");
+// var a: number = 20;
+// var b = "Abu Hurairah";
+// console.log(b);
+// Store your name and age in a variable and show as following
+// My name is Abu Hurairah and I am 21 years old.
+var userName = "Asaam";
+var userAge = 19;
+console.log("My name is", userName, "I am", userAge, "years old.");
---
+++ index.ts
@@ -0,0 +1,14 @@
+console.log("Hello World!");
+// console.log("Hello world!");
+// var a: number = 20;
+// var b = "Abu Hurairah";
+// console.log(b);
+
+// Store your name and age in a variable and show as following
+
+// My name is Abu Hurairah and I am 21 years old.
+
+var userName: string = "Asaam";
+var userAge: number = 19;
+
+console.log("My name is", userName, "I am", userAge, "years old.");
